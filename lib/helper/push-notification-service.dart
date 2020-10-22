5aca1fd6a0b96023507de3df134bed8f91043a87
Ben <benbright065@gmail.com>
2020-10-22 04:46:30 +0100

    update location condition

--- lib/helper/push-notification-service.dart
+++ lib/helper/push-notification-service.dart
@@ -1,36 +1,50 @@
 import 'dart:io';
-
-import 'package:alpha_drivers/utils/global-variables.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService{
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-Future initialize() async{
-  _firebaseMessaging.configure(
-    onMessage: (Map<String, dynamic> message) async {
-      print("onMessage: $message");
-      if(Platform.isAndroid){
+  Future initialize() async{
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        getRideId(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        getRideId(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        getRideId(message);
+      },
+    );
+  }
+  Future<String> getToken() async{
+    String token = await _firebaseMessaging.getToken();
+    print('token got as: $token');
+    // DatabaseReference tokenRef = FirebaseDatabase. instance.reference()
+    //     .child('drivers/${currentFirebaseUser.uid}/token');
+    // tokenRef.set(token);
+    _firebaseMessaging.subscribeToTopic('alldrivers');
+    _firebaseMessaging.subscribeToTopic('allusers');
+
+  }
+  String getRideId(Map<String, dynamic> message){
+    String rideId = '';
+    print("onMessage: $message");
+    if(Platform.isAndroid){
       String rideId = message['data']['ride_id'];
+      print('ride_id: $rideId');
+    } else{
+      String rideId = message['ride_id'];
+      print('ride_id: $rideId');
+    }
+  }
+  void fetchRideInfo(String rideId){
+ DatabaseReference rideRef = FirebaseDatabase.instance
+     .reference().child('rideRequest/$rideId');
+ rideRef.once().then((DataSnapshot snapshot){
 
-      }
-    },
-    onLaunch: (Map<String, dynamic> message) async {
-      print("onLaunch: $message");
-    },
-    onResume: (Map<String, dynamic> message) async {
-      print("onResume: $message");
-    },
-  );
-}
-Future<String> getToken() async{
-  String token = await _firebaseMessaging.getToken();
-  print('token got as: $token');
-  DatabaseReference tokenRef = FirebaseDatabase. instance.reference()
-  .child('drivers/${currentFirebaseUser.uid}/token');
-  tokenRef.set(token);
-  _firebaseMessaging.subscribeToTopic('alldrivers');
-  _firebaseMessaging.subscribeToTopic('allusers');
-
-}
+ });
+  }
 }
