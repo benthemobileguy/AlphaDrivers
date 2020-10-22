eae34c039b29931355ec708b1e6827b6d55cd884
Ben <benbright065@gmail.com>
2020-10-22 05:39:45 +0100

    update location condition

---
+++ lib/screens/components/notification-dialog.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class NotificationDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+
+    );
+  }
+}
