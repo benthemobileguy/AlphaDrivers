cd6ea39833a7fbec2571678781f3984bbffbe3ab
Ben <benbright065@gmail.com>
2020-10-22 05:42:40 +0100

    update location condition

--- lib/screens/components/notification-dialog.dart
+++ lib/screens/components/notification-dialog.dart
@@ -3,7 +3,24 @@ class NotificationDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
+   shape: RoundedRectangleBorder(
+     borderRadius:  BorderRadius.circular(10.0),
+   ),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child:  Container(
+        margin: EdgeInsets.all(4),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: Column(
+          children: <Widget>[
 
+          ],
+        ),
+      ),
     );
   }
 }
