189d4989aac07018680d1c39a98ef2bcdf76d07d
Ben <benbright065@gmail.com>
2020-10-23 00:55:32 +0100

    update location condition

---
+++ lib/screens/new-trip-page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class NewTripPage extends StatefulWidget {
+  @override
+  _NewTripPageState createState() => _NewTripPageState();
+}
+
+class _NewTripPageState extends State<NewTripPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Trip'),
+      ),
+    );
+  }
+}
