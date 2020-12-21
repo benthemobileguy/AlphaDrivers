d0fe1ae810775336d5496552e4a4ce77e29ea21e
Ben <benbright065@gmail.com>
2020-12-21 12:29:07 +0100

    Earnings Screen

--- lib/tabs/weekly-earnings-tab.dart
+++ lib/tabs/weekly-earnings-tab.dart
@@ -4,35 +4,43 @@ import 'package:alpha_drivers/screens/components/custom-balance.dart';
 import 'package:alpha_drivers/theme/style.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+
 class WeeklyEarningsTab extends StatefulWidget {
   @override
   _WeeklyEarningsTabState createState() => _WeeklyEarningsTabState();
 }
 
 class _WeeklyEarningsTabState extends State<WeeklyEarningsTab> {
-final List<EarningsChart> data = [
-  EarningsChart(day :"Sun",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.black38), days: 1),
-  EarningsChart(day :"Mon",
-      barColor:
-charts.ColorUtil.fromDartColor(Colors.blue), days: 2),
-  EarningsChart(day :"Tues",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.orange), days: 3),
-  EarningsChart(day :"Wed",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.green), days: 4),
-  EarningsChart(day :"Thurs",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.pink), days: 5),
-  EarningsChart(day :"Frid",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.brown), days: 6),
-  EarningsChart(day :"Sat",
-      barColor:
-      charts.ColorUtil.fromDartColor(Colors.purple), days: 7),
-];
+  final List<EarningsChart> data = [
+    EarningsChart(
+        day: "Sun",
+        barColor: charts.ColorUtil.fromDartColor(Colors.black38),
+        days: 1),
+    EarningsChart(
+        day: "Mon",
+        barColor: charts.ColorUtil.fromDartColor(Colors.blue),
+        days: 2),
+    EarningsChart(
+        day: "Tues",
+        barColor: charts.ColorUtil.fromDartColor(Colors.orange),
+        days: 3),
+    EarningsChart(
+        day: "Wed",
+        barColor: charts.ColorUtil.fromDartColor(Colors.green),
+        days: 4),
+    EarningsChart(
+        day: "Thurs",
+        barColor: charts.ColorUtil.fromDartColor(Colors.pink),
+        days: 5),
+    EarningsChart(
+        day: "Frid",
+        barColor: charts.ColorUtil.fromDartColor(Colors.brown),
+        days: 6),
+    EarningsChart(
+        day: "Sat",
+        barColor: charts.ColorUtil.fromDartColor(Colors.purple),
+        days: 7),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +51,6 @@ charts.ColorUtil.fromDartColor(Colors.blue), days: 2),
             SizedBox(
               height: 20,
             ),
-
             Center(
               child: Text(
                 "Mon. 27th July 2020",
@@ -66,16 +73,18 @@ charts.ColorUtil.fromDartColor(Colors.blue), days: 2),
             SizedBox(
               height: 20,
             ),
-         SizedBox(
-           height: 300,
-           child: BarChartEarnings(
-             data: data,
-           ),
-         ),
+            SizedBox(
+              height: 300,
+              child: BarChartEarnings(
+                data: data,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
             Divider(
               color: dividerColor,
               thickness: 1,
-
             ),
             IntrinsicHeight(
               child: Row(
@@ -275,11 +284,9 @@ charts.ColorUtil.fromDartColor(Colors.blue), days: 2),
                 ],
               ),
             ),
-
             Divider(
               color: dividerColor,
               thickness: 1,
-
             ),
             Padding(
               padding: const EdgeInsets.all(14.0),
